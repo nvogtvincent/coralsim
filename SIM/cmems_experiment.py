ce8bb4465a7c8bcbd79159bd55243095b1cb272f
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2022-03-17 20:43:12 +0000

    Quick updates to scripts

--- SIM/cmems_experiment.py
+++ SIM/cmems_experiment.py
@@ -35,13 +35,24 @@ test        = False # Either False (no test), traj (trajectory test), kernel (ke
 # Create experiment object
 experiment = Experiment()
 
-# Import grid parameters
+# Run experiment
 experiment.config(dirs, preset='CMEMS')
 experiment.generate_fieldset(interp_method='freeslip')
 experiment.generate_particleset(num=pn_per_cell, t0=t0, filters={'eez': [690]},
-                                competency=timedelta(days=5),
+                                min_competency=timedelta(days=2),
                                 dt=dt, run_time=run_time, test=test,
                                 plot='grp', plot_fh='pos0_sey.png',)
 
 experiment.run(fh='example_experiment.nc')
+
+# Analyse output
+
+
+
+
+
+
+
+
+
 
--- SIM/cmems_export.py
+++ SIM/cmems_export.py
@@ -11,51 +11,59 @@ import matplotlib.pyplot as plt
 import cmocean.cm as cm
 from netCDF4 import Dataset, num2date
 from datetime import datetime, timedelta
-from glob import glob
 from sys import argv
-from coralsim import Output
+from coralsim import Experiment
 
 ##############################################################################
 # DEFINE INPUT FILES                                                         #
 ##############################################################################
 
 # PARAMETERS
-mortality_ts = 14
-settling_ts = 1
+mortality_t = 14         # Mortality timescale (d)
+settling_t = 1           # Settling timescale (d)
+mean_competency_t = 7    # Time at which 50% of larvae are competent (d)
+competency_t_spread = 1  # Spread of logistic function for competency (d)
 
-lm = 1/(mortality_ts*24*3600) # Mortality rate (1/s)
-ls = 1/(settling_ts*24*3600)  # Settling rate (1/s)
+parameters = {'lm': 1/(mortality_t*24*3600),
+              'ls': 1/(settling_t*24*3600),
+              'tc': mean_competency_t*24*3600,
+              'kc': 1/(competency_t_spread*24*3600)}
 
 # DIRECTORIES
 dirs = {}
 dirs['root'] = os.getcwd() + '/../'
+dirs['model'] = dirs['root'] + 'MODEL_DATA/CMEMS/'
 dirs['grid'] = dirs['root'] + 'GRID_DATA/'
-dirs['traj'] = dirs['root'] + 'TRAJ/'
 dirs['fig'] = dirs['root'] + 'FIGURES/'
+dirs['traj'] = dirs['root'] + 'TRAJ/'
 
 # FILE HANDLES
-fh = {}
-fh['traj'] = sorted(glob(dirs['traj'] + 'cmems6400*.nc'))
-fh['grid'] = dirs['grid'] + 'coral_grid.nc'
-fh['out'] = dirs['traj'] + 'data_m' + str(mortality_ts) + '_s' + str(settling_ts) + '.pkl'
-fh['fig'] = dirs['fig'] + 'connectivity_matrix_m' + str(mortality_ts) + '_s' + str(settling_ts) + '.png'
+# fh = {}
+# fh['traj'] = sorted(glob(dirs['traj'] + 'cmems6400*.nc'))
+# fh['grid'] = dirs['grid'] + 'coral_grid.nc'
+# fh['out'] = dirs['traj'] + 'data_m' + str(mortality_ts) + '_s' + str(settling_ts) + '.pkl'
+# fh['fig'] = dirs['fig'] + 'connectivity_matrix_m' + str(mortality_ts) + '_s' + str(settling_ts) + '.png'
 
 ##############################################################################
 # FORMAT OUTPUT                                                              #
 ##############################################################################
 
-seychelles_output = Output(dirs['root'])
+sey = Experiment()
+sey.config(dirs, preset='CMEMS', dt=timedelta(hours=1),
+           releases_per_month=1, larvae_per_cell=6400)
+sey.generate_dict()
+sey.to_dataframe(fh='cmems6400*', parameters=parameters)
 
-# Generate dictionaries
-seychelles_output.generate_dict(fh['grid'],
-                                idx_varname='coral_idx_c',
-                                cf_varname='coral_frac_c',
-                                cc_varname='coral_cover_c',
-                                grp_varname='coral_grp_c',)
+# # Generate dictionaries
+# sey.generate_dict(fh['grid'],
+#                                 idx_varname='coral_idx_c',
+#                                 cf_varname='coral_frac_c',
+#                                 cc_varname='coral_cover_c',
+#                                 grp_varname='coral_grp_c',)
 
-seychelles_output.to_dataframe(fh['traj'], lm=lm, ls=ls, dt=3600.,
-                               lpc=6400, rpm=3, matrix=True)
+# seychelles_output.to_dataframe(fh['traj'], lm=lm, ls=ls, dt=3600.,
+#                                lpc=6400, rpm=3, matrix=True)
 
-# seychelles_output.data.to_pickle(fh['out'])
+# # seychelles_output.data.to_pickle(fh['out'])
 
-seychelles_output.export_matrix(fh['fig'], scheme='seychelles', n_years=27)
+# seychelles_output.export_matrix(fh['fig'], scheme='seychelles', n_years=27)
